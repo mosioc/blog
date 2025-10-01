b97cce3eb37bc2c5128eef4a62876429bfb55784
mosioc <mosioc79@gmail.com>
2025-10-02 01:17:15 +0330

    comment out base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://mosioc.github.io/blog/",
-  base: "/blog/",
+  // base: "/blog/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
