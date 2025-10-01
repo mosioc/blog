163ad3b2eed1de32b3e24109982fb04db8140e0c
mosioc <mosioc79@gmail.com>
2025-10-02 00:37:11 +0330

    change base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://mosioc.github.io/blog/",
-  base: "/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  // base: "/",
+  integrations: [mdx(), sitemap(), tailwind()]
 });
